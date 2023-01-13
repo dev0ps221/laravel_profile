01841ea42102285cd63b34bbdb21769a78d83290
dev0ps221 <seybalofficiel@gmail.com>
2023-01-13 18:36:04 +0000

    a bit of laravel

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -5,4 +5,8 @@
         view('contactmessage')
     }}
 
+    {{
+        view('joinme')
+    }}
+
 </section>
--- resources/views/joinme.blade.php
+++ resources/views/joinme.blade.php
@@ -1,4 +1,27 @@
 <link rel="stylesheet" href="{{URL::asset('/css/joinme.css')}}">
 <section id="joinme">
-    
+    <h4>
+        vous pouvez utilisez les coordonnees ci-dessus pour me joindre
+    </h4>
+    <div id="infos">
+        <div class="phoneinfos infobox">
+            <label for="tel">
+                numero de telephone
+            </label>
+            <span class='tel'>
+                78 426 83 44
+            </span>
+        </div>
+        <div class="mailinfos infobox">
+            <label for="mail">
+                adresses e-mail
+            </label>
+            <span class='mail'>
+                seybalofficiel@gmail.com
+            </span>
+            <span class='mail'>
+                seybaxscorpion@gmail.com
+            </span>
+        </div>
+    </div>
 </section>
