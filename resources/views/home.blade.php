df390d8b64eec8c53aebb7b8790ee2c5f3d1e0d9
dev0ps221 <seybalofficiel@gmail.com>
2023-01-13 01:59:57 +0000

    a bit of laravel

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,4 +1,15 @@
 
     <link rel="stylesheet" href="{{ URL::asset('css/home.css') }}" />
     
-    
+    <section id="shortabout">
+        <h2>
+            @dev0ps221
+        </h2>
+        <p>
+            Je suis un passionne des technologies depuis l'enfance, et j'adore les défis.
+            Je ne me cherche pas de limites en termes de compétences, et tout projet ou travail
+            requérant l'informatique la programmation ou une quelconque orientation de ces sujets me convient.
+            Visons l 'excellence et la productivité.
+            A bientôt pour des partenariats efficaces et fructueux.
+        </p>
+    </section>
--- resources/views/top.blade.php
+++ resources/views/top.blade.php
@@ -1,20 +1,18 @@
 <link rel="stylesheet" href="{{URL::asset("css/top.css")}}"/>
-<header>
-    <nav>
-        <div id="headline">
-            <h1 id="logobox">
-                El Hadji Seybatou Mbengue
-            </h1>
-            <h4>
-                Developpeur et feru de technologies
-            </h4>
+<nav>
+    <div id="headline">
+        <h1 id="logobox">
+            El Hadji Seybatou Mbengue
+        </h1>
+        <h4>
+            Developpeur et feru de technologies
+        </h4>
 
-        </div>
-        <div class="list">
-            <a href="/">moi</a>
-            <a href="/projets">mes projets</a>
-            <a href="/blog">posts</a>
-            <a href="/message">ecrivez moi</a>
-        </div>
-    </nav>
-</header>
+    </div>
+    <div class="list">
+        <a href="/">moi</a>
+        <a href="/projets">mes projets</a>
+        <a href="/blog">posts</a>
+        <a href="/message">ecrivez moi</a>
+    </div>
+</nav>
