39c8ff816e7adf9465414901762cc14ee100a43b
dev0ps221 <seybalofficiel@gmail.com>
2023-01-13 01:14:08 +0000

    a bit of laravel

--- resources/views/top.blade.php
+++ resources/views/top.blade.php
@@ -1,21 +1,20 @@
 <link rel="stylesheet" href="{{URL::asset("css/top.css")}}"/>
-<nav>
-    <div id="headline">
-        <h1 id="logobox">
-            El Hadji Seybatou Mbengue
-        </h1>
-        <h4>
-            Developpeur et feru de technologies
-        </h4>
-
-    </div>
-    <div class="list">
-        <a href="/">page</a>
-        <a href="/projets">page</a>
-        <a href="/blog">page</a>
-        <a href="/message">page</a>
-        <a href="/">page</a>
-    </div>
-</nav>
 <header>
+    <nav>
+        <div id="headline">
+            <h1 id="logobox">
+                El Hadji Seybatou Mbengue
+            </h1>
+            <h4>
+                Developpeur et feru de technologies
+            </h4>
+
+        </div>
+        <div class="list">
+            <a href="/">moi</a>
+            <a href="/projets">mes projets</a>
+            <a href="/blog">posts</a>
+            <a href="/message">ecrivez moi</a>
+        </div>
+    </nav>
 </header>
