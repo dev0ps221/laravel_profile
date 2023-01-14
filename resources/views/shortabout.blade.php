012ac3a32a1e7e4c749dd0b23554199f05a7a350
dev0ps221 <seybalofficiel@gmail.com>
2023-01-14 10:11:14 +0000

    a bit of laravel

--- resources/views/shortabout.blade.php
+++ resources/views/shortabout.blade.php
@@ -5,8 +5,8 @@
     <img src="/pics/seybatou-mbengue.jpg" alt="El Hadji Seybatou Mbengue" id="selfpic">
     <p>
         Je suis un passionne des technologies depuis l'enfance, et j'adore les défis.
-        Je ne me cherche pas de limites en termes de compétences, et tout projet ou travail
-        requérant l'informatique la programmation ou une quelconque orientation de ces sujets me convient.
+        Je ne me cherche pas de limites en termes de compétences et je porte un sensible interet pour tout projet ou travail
+        requérant l'informatique la programmation ou une quelconque orientation de ces sujets.
         Visons l 'excellence et la productivité.
         A bientôt pour des partenariats efficaces et fructueux.
     </p>
